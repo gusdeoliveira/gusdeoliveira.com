d99529929186645cbcd8b9583e4aa5fff174e6f4
Gustavo de Oliveira <gustavolive22@gmail.com>
2026-01-06 02:28:37 -0300

    feat: added lang parameter for english links

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ const languages = {
     }
 };
 
+function getLangFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    const paramLang = params.get('lang');
+    if (paramLang && languages[paramLang]) {
+        return paramLang;
+    }
+
+    const path = window.location.pathname.toLowerCase();
+    const match = path.match(/^\/(en|pt)(?:\/|$)/);
+    return match ? match[1] : null;
+}
+
+const urlLang = getLangFromUrl();
+if (urlLang) {
+    currentLang = urlLang;
+    localStorage.setItem('preferred-language', currentLang);
+}
+
 const themeIcons = {
     light: `<svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
   <circle cx="12" cy="12" r="4"/>
